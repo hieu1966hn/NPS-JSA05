157240e2f5a9c4b6e4a84551fee3676c279218f0
hieu1966hn <hieu1966hn@gmail.com>
2021-10-21 20:10:14 +0700

    indexof

--- SS5/index.js
+++ SS5/index.js
@@ -9,7 +9,7 @@
 ///khai báo: 
 // let animals = [];  //C2
 
-let animals = new Array("dog", "cat", "fish");
+let animals = new Array("dog", "cat", "fish", "cat");
 
 // console.log(animals);
 
@@ -26,8 +26,25 @@ let animals = new Array("dog", "cat", "fish");
 //   console.log(animals[i]);
 // }
 
+// C2: sử dụng for-of
+// for (let key of animals) {
+//   console.log(key); // ra gì?? 
+// }
+
+// => for of => biến chạy chính là giá trị phần tử của mảng
+
+// C3: sử dụng for - in
+
+// for (let index in animals) {
+//   console.log(index);// in ra gì?? in ra vị trí phần tử mảng
+// }
+
+// => for of => biến chạy chính là vị trí phần tử của mảng
 
 
+// Phương thức IndexOf trong mảng: dùng để tìm kiếm vị trí phần tử trong mảng (dựa vào giá trị);
+// Tìm vị trí chú mèo "cat" bên trong mảng. Ta sử dụng indexOf
+let vitriMeo = animals.indexOf("cat");// tìm cat bên trong mảng và trả về vị trí đầu tiên tìm thấy
+console.log(vitriMeo);
 
-
-
+//  Tìm vị trí chú mèo thứ 2 trong mảng???
