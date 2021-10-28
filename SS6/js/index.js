7891b374471fa429ff03b4afcdf03ea26d3cf7ec
hieu1966hn <hieu1966hn@gmail.com>
2021-10-28 21:34:48 +0700

    done front-end weather API city

---
+++ SS6/js/index.js
@@ -0,0 +1,10 @@
+const APP_ID = '651daf0af47eb49a9b65a0007afcce53';
+const DEFAULT_VALUE = "_ _";
+
+const searchInput = document.querySelector('#search-input');
+const cityName = document.querySelector('.city-name');
+const weatherState = document.querySelector('.weather-state')
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+
+
